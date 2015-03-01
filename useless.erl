7333469c50541211b6c54c7c95f4a19a9c0b8cde
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-03-01 21:05:29 +0100

    added author of file

--- useless.erl
+++ useless.erl
@@ -1,8 +1,13 @@
 -module(useless).
--export ([add/2, hello/0]).
+-export([add/2, hello/0, greet_and_add_two/1]).
+-author("Mateusz Maciaszek").
 
 add(A,B) ->
   A + B.
 
 hello() ->
-  io:format("Hello, word!~n").
+  io:format("Hello, world!~n").
+
+greet_and_add_two(X) ->
+  hello(),
+  add(X,2).
